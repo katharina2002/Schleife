35c691e36912871172f6dcd5d98de4491c6735de
Katharina <katharina@wolf-wide-web.de>
2024-09-16 22:11:17 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * 13.07.2023 Katharina Wolf
+ * Einfache Schleife
+ */
+Console.WriteLine("Wir zählen von 0 bis 20 hoch und wieder zurück auf 0");
+Console.WriteLine("Wo starten wir?");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Wie weit zählen wir?");
+int ende = Convert.ToInt32(Console.ReadLine());
+
+int zahl = start;
+while (zahl < ende)
+{
+    Console.WriteLine("{0}", zahl);
+    zahl++;
+}
+
+int zahl2 = ende;
+do
+{
+    Console.WriteLine("{0}", zahl2);
+    zahl2--;
+} while (zahl2 >= start);
